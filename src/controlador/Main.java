9100d7913c8ace20009c2c455d785e1613485d16
davic <davic@DESKTOP-9OM2L87>
2023-06-17 20:21:27 -0500

    Carga Inicial

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -1,9 +1,14 @@
 package controlador;
-
 import vista.Principal;
+import vista.visConductor;
 
 public class Main {
     public static void main(String [] args){
         Principal p = new Principal();
+        ctrlPrincipal ctrl_prin = new ctrlPrincipal(p);
+        ctrl_prin.control();
+        visConductor visconductor = new visConductor();
+        visconductor.setVisible(true);
+        p.getJdp_principal().add(visconductor);
     }
 }
