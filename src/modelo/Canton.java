40fd1192594d31de9925ba24f5a66fa7bd7b5c3b
davic <davic@DESKTOP-9OM2L87>
2023-06-17 20:53:28 -0500

    Carga Inicial

--- src/modelo/Canton.java
+++ src/modelo/Canton.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package modelo;
 
-/**
- *
- * @author davic
- */
+package modelo;
 public class Canton {
     private int codigo;
     String nombre;
--- src/modelo/Envio.java
+++ src/modelo/Envio.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author davic
- */
+import java.util.Date;
+
 public class Envio {
+    private int codigo;
+    private Date fecha;
+    private int codigo_vista;
+
+    public Envio() {
+    }
+
     
+    
+    public Envio(int codigo, Date fecha, int codigo_vista) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.codigo_vista = codigo_vista;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCodigo_vista() {
+        return codigo_vista;
+    }
+
+    public void setCodigo_vista(int codigo_vista) {
+        this.codigo_vista = codigo_vista;
+    }
+
+    @Override
+    public String toString() {
+        return "Envio{" + "codigo=" + codigo + ", fecha=" + fecha + ", codigo_vista=" + codigo_vista + '}';
+    }
 }
